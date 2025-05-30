fee0f40432be932c9282b13aac691aa1c074a0d8
bb <bbelek06@gmail.com>
2025-05-30 19:40:51 +0700

    Game

---
+++ Gallows/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Game
+{
+    public string Word { get; private set; }
+    public HashSet<char> GuessedLetters { get; private set; } = new HashSet<char>();
+    public int Errors { get; private set; } = 0;
+    public int MaxErrors { get; private set; } = 6;
+
+    public Game(string word)
+    {
+        Word = word.ToUpper();
+        GuessedLetters.Clear();
+        Errors = 0;
+    }
+
+    public bool CheckLetter(char letter)
+    {
+        letter = Char.ToUpper(letter);
+        if (GuessedLetters.Contains(letter))
+            return false; // уже угадывали
+
+        GuessedLetters.Add(letter);
+
+        if (!Word.Contains(letter))
+        {
+            Errors++;
+            return false;
+        }
+        return true;
+    }
+
+    public bool IsWin()
+    {
+        foreach (var ch in Word)
+        {
+            if (!GuessedLetters.Contains(ch))
+                return false;
+        }
+        return true;
+    }
+
+    public bool IsLose()
+    {
+        return Errors >= MaxErrors;
+    }
+
+    public void Reset(string newWord)
+    {
+        Word = newWord.ToUpper();
+        GuessedLetters.Clear();
+        Errors = 0;
+    }
+}
