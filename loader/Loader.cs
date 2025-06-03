2a47c4f57dc3df4355ad5c7ffe2401c53facaa22
admin <tbd82@mail.ru>
2025-06-03 21:58:58 +0700

    feat

---
+++ loader/Loader.cs
@@ -0,0 +1,36 @@
+﻿
+// подключаем библиотеку для работы с файлами
+//using System.Collections.Generic;
+//using System.IO;
+
+//namespace ConsoleApplication1
+//    {
+//        class Program1
+//        {
+//            static void Main(string[] args)
+//            {
+//                string filePath = "//1.txt"; // имя файла
+//            string str=" ";
+
+//            List<string> zs = new List<string>();
+//            zs.Add ("Deer");
+//            zs.Add("Bear");
+//            zs.Add("Swallow");
+//            zs.Add("Rabbit");
+
+
+
+//            // открываем файл если нет файла то создаем файл
+//            using (StreamWriter fileStream = File.Exists(filePath) ? File.AppendText(filePath) : File.CreateText(filePath))
+//            {
+//                foreach (string s in zs)
+//                {
+//                    fileStream.WriteLine(zs[0]);
+//                }
+//            }
+//            }
+//        }
+//    }
+
+
+
